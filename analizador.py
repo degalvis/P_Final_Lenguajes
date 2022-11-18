9aa6d022f1c930eef9793c46466017f46ae22a84
julimejia <davidmejiao43@gmail.com>
2022-11-18 17:50:05 -0500

    cambios en la cantidad de ciclos

--- analizador.py
+++ analizador.py
@@ -8,30 +8,7 @@ class analizador:
         sujetos = ["carlos", "anna", "maria", "diego", "julian", "el niño", "la niña"]
         verbos = ["corre", "salta", "camina", "come", "pela", "salta", "juega"]  
         predicados = ["en el parque", "en la cancha", "en el colegio", "en un trampolin", ""] 
-        bool = False
-        boolSujeto = False
-        boolverbo = False
-        boolpredicado = False 
-        
-       
-        for i in range(0, len(sujetos)-1):
-            if self.sujeto == sujetos[i]:
-                boolSujeto = True
-                break
-            
-        for i in range(0, len(verbos)-1):
-            if self.verbo == verbos[i]:
-                boolverbo = True
-                break
-        
-        for i in range(0, len(predicados)-1):
-            if self.predicado == predicados[i]:
-                boolpredicado = True
-                break
-            
-        if boolSujeto == True and boolpredicado == True and boolverbo == True:
-            bool = True
-        
+        bool = sujetos.__contains__(self.sujeto) and verbos.__contains__(self.verbo) and predicados.__contains__(self.predicado)
         return bool
         
 
